6343f738f686032494d6852dfa603d72b1a9e9ee
Jonny Rodriguez <jojoroto@gmail.com>
2019-03-20 00:16:30 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,3 +1,3 @@
 var playlist = { "pepe": "pepe song" };
 var updatePlaylist = (playlist, artistName, songTitle) => playlist[artistName]=songTitle;
-var removeFromPlaylist = (playlist, artistName) => delete playlist
+var removeFromPlaylist = (playlist, artistName) => delete playlist[artistName];
